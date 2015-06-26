da6e4300725d00f6505a4ed8fcd8dfbee2cfefb1
owenWU <owen.wu@ef.com>
2015-06-26 13:33:15 +0800

    Update the AudioRecorder

--- ios-pocketsphinx-o/ViewController.m
+++ ios-pocketsphinx-o/ViewController.m
@@ -11,157 +11,138 @@
 #import <AudioToolbox/AudioToolbox.h>
 #import <AVFoundation/AVFoundation.h>
 
-@interface ViewController () <AVAudioRecorderDelegate>
+@interface ViewController () <AVAudioRecorderDelegate, AVAudioPlayerDelegate>
 {
     AVAudioRecorder *recorder;
-    NSTimer *timer;
-    NSURL *urlPlay;
-    
+    AVAudioPlayer *player;
 }
-@property (retain, nonatomic) IBOutlet UIButton *btn;
-@property (retain, nonatomic) IBOutlet UIImageView *imageView;
-@property (retain, nonatomic) IBOutlet UIButton *playBtn;
-@property (retain, nonatomic) AVAudioPlayer *avPlay;
+
+@property (weak, nonatomic) IBOutlet UIButton *recordPauseButton;
+@property (weak, nonatomic) IBOutlet UIButton *stopButton;
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+
+- (IBAction)recordPauseTapped:(id)sender;
+- (IBAction)stopTapped:(id)sender;
+- (IBAction)playTapped:(id)sender;
 @end
 
 @implementation ViewController
-
-@synthesize btn;
-@synthesize imageView = _imageView;
-@synthesize playBtn = _playBtn;
-@synthesize avPlay = _avPlay;
+@synthesize stopButton, playButton, recordPauseButton;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    [self audio];
-    [self.btn addTarget:self action:@selector(btnDown:) forControlEvents:UIControlEventTouchDown];
-    [self.btn addTarget:self action:@selector(btnUp:) forControlEvents:UIControlEventTouchUpInside];
-    [self.btn addTarget:self action:@selector(btnDragUp:) forControlEvents:UIControlEventTouchDragExit];
-    [self.playBtn addTarget:self action:@selector(playRecordSound:) forControlEvents:UIControlEventTouchDown];
-}
-
-- (IBAction)playRecordSound:(id)sender
-{
-    if (self.avPlay.playing) {
-        [self.avPlay stop];
-        return;
-    }
-    AVAudioPlayer *player = [[AVAudioPlayer alloc]initWithContentsOfURL:urlPlay error:nil];
-    self.avPlay = player;
-    [self.avPlay play];
-}
-
-- (IBAction)btnDown:(id)sender
-{
-    //创建录音文件，准备录音
-    if ([recorder prepareToRecord]) {
-        //开始
-        [recorder record];
+    
+    
+    [stopButton setEnabled:NO];
+    [playButton setEnabled:NO];
+    
+    
+    // set Audio file path
+    NSArray *pathComponents = [NSArray arrayWithObjects:
+                               [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
+                               @"MyAudioMemo.wav",
+                               nil];
+    NSURL* outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
+    
+    // setup audio session
+    NSError *setOverrideError;
+    NSError *setCategoryError;
+    
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:&setCategoryError];
+    
+    if(setCategoryError){
+        NSLog(@"%@", [setCategoryError description]);
     }
     
-    //设置定时检测
-    timer = [NSTimer scheduledTimerWithTimeInterval:0 target:self selector:@selector(detectionVoice) userInfo:nil repeats:YES];
-}
-- (IBAction)btnUp:(id)sender
-{
-    double cTime = recorder.currentTime;
-    if (cTime > 2) {//如果录制时间<2 不发送
-        NSLog(@"发出去");
-    }else {
-        //删除记录的文件
-        [recorder deleteRecording];
-        //删除存储的
+    // increase the playing voice when play recorded voice
+    [session overrideOutputAudioPort:AVAudioSessionPortOverrideSpeaker error:&setOverrideError];
+    
+    
+    if(setOverrideError){
+        NSLog(@"%@", [setOverrideError description]);
     }
-    [recorder stop];
-    [timer invalidate];
-}
-- (IBAction)btnDragUp:(id)sender
-{
-    //删除录制文件
-    [recorder deleteRecording];
-    [recorder stop];
-    [timer invalidate];
     
-    NSLog(@"取消发送");
-}
-- (void)audio
-{
-    //录音设置
-    NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc]init];
-    //设置录音格式  AVFormatIDKey==kAudioFormatLinearPCM
-    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
-    //设置录音采样率(Hz) 如：AVSampleRateKey==8000/44100/96000（影响音频的质量）
-    [recordSetting setValue:[NSNumber numberWithFloat:44100] forKey:AVSampleRateKey];
-    //录音通道数  1 或 2
-    [recordSetting setValue:[NSNumber numberWithInt:1] forKey:AVNumberOfChannelsKey];
-    //线性采样位数  8、16、24、32
-    [recordSetting setValue:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
-    //录音的质量
-    [recordSetting setValue:[NSNumber numberWithInt:AVAudioQualityHigh] forKey:AVEncoderAudioQualityKey];
+    // define the recorder setting
+    NSMutableDictionary* recorderSetting = [[NSMutableDictionary alloc] init];
+    [recorderSetting setValue:[NSNumber numberWithInt:kAudioFormatLinearPCM] forKey:AVFormatIDKey];
+    [recorderSetting setValue:[NSNumber numberWithFloat:8000.0] forKey:AVSampleRateKey];
+    [recorderSetting setValue:[NSNumber numberWithInt:1] forKey:AVNumberOfChannelsKey];
     
-    NSString *strUrl = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-    NSURL *url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@/lll.aac", strUrl]];
-    urlPlay = url;
-    
-    NSError *error;
-    //初始化
-    recorder = [[AVAudioRecorder alloc]initWithURL:url settings:recordSetting error:&error];
-    //开启音量检测
-    recorder.meteringEnabled = YES;
+    // initiate and prepare the recorder
+    recorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recorderSetting error:nil];
     recorder.delegate = self;
+    recorder.meteringEnabled = YES;
+    [recorder prepareToRecord];
 }
 
-- (void)detectionVoice
-{
-    [recorder updateMeters];//刷新音量数据
-    //获取音量的平均值  [recorder averagePowerForChannel:0];
-    //音量的最大值  [recorder peakPowerForChannel:0];
-    
-    double lowPassResults = pow(10, (0.05 * [recorder peakPowerForChannel:0]));
-    NSLog(@"%lf",lowPassResults);
-    //最大50  0
-    //图片 小-》大
-    if (0<lowPassResults<=0.06) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_01.png"]];
-    }else if (0.06<lowPassResults<=0.13) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_02.png"]];
-    }else if (0.13<lowPassResults<=0.20) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_03.png"]];
-    }else if (0.20<lowPassResults<=0.27) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_04.png"]];
-    }else if (0.27<lowPassResults<=0.34) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_05.png"]];
-    }else if (0.34<lowPassResults<=0.41) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_06.png"]];
-    }else if (0.41<lowPassResults<=0.48) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_07.png"]];
-    }else if (0.48<lowPassResults<=0.55) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_08.png"]];
-    }else if (0.55<lowPassResults<=0.62) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_09.png"]];
-    }else if (0.62<lowPassResults<=0.69) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_10.png"]];
-    }else if (0.69<lowPassResults<=0.76) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_11.png"]];
-    }else if (0.76<lowPassResults<=0.83) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_12.png"]];
-    }else if (0.83<lowPassResults<=0.9) {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_13.png"]];
-    }else {
-        [self.imageView setImage:[UIImage imageNamed:@"record_animate_14.png"]];
-    }
-}
-
-- (void) updateImage
-{
-    [self.imageView setImage:[UIImage imageNamed:@"record_animate_01.png"]];
-}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)recordPauseTapped:(id)sender {
+    // stop the audio player before recording
+    if ( player.playing)
+        [player stop];
+    
+    if(!recorder.recording) {
+        AVAudioSession *session = [AVAudioSession sharedInstance];
+        //[session setCategory:AVAudioSessionCategoryRecord error:nil];
+        [session setActive:YES error:nil];
+        
+        // start recording
+        [recorder record];
+        [recordPauseButton setTitle:@"Pause" forState:UIControlStateNormal];
+    } else {
+        // pause recording
+        [recorder pause];
+        [recordPauseButton setTitle:@"Record" forState:UIControlStateNormal];
+    }
+    
+    [stopButton setEnabled:YES];
+    [playButton setEnabled:NO];
+}
+
+- (IBAction)stopTapped:(id)sender {
+    [recorder stop];
+    
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setActive:NO error:nil];
+}
+
+- (IBAction)playTapped:(id)sender {
+    if(!recorder.recording) {
+        //        AVAudioSession* session = [AVAudioSession sharedInstance];
+        //        [session setCategory:AVAudioSessionCategoryPlayback error:nil];
+        
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:recorder.url error:nil];
+        [player setDelegate:self];
+        [player play];
+        
+    }
+}
+
+#pragma mark -
+#pragma mark AVAudioRecorderDelegate
+- (void) audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder successfully:(BOOL)flag {
+    
+    [recordPauseButton setTitle:@"Record" forState:UIControlStateNormal];
+    [stopButton setEnabled:NO];
+    [playButton setEnabled:YES];
+}
+
+
+#pragma mark -
+#pragma mark AVAudioPlayerDelegate
+- (void) audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Done"
+                                                    message: @"Finish playing the recording!"
+                                                   delegate: nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
 @end
